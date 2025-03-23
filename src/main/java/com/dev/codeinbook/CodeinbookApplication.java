3e68c4de7020aed913547b6ac97e6949af3a5053
anailson <arsantos315@gmail.com>
2025-03-23 15:59:49 -0300

    feat: estrutura do projeto arquitetura hexagonal

---
+++ src/main/java/com/dev/codeinbook/CodeinbookApplication.java
@@ -0,0 +1,14 @@
+package com.dev.codeinbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+CodeinbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeinbookApplication.class, args);
+	}
+
+}
