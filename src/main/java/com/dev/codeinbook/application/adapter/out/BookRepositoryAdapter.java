b0fe85bf7a7c0824f4a96f899c83aa7511ea1dbb
anailson <arsantos315@gmail.com>
2025-04-04 21:01:23 -0300

    Implementando o adaptador secundário  e PageRequest

--- src/main/java/com/dev/codeinbook/application/adapter/out/BookRepositoryAdapter.java
+++ src/main/java/com/dev/codeinbook/application/adapter/out/BookRepositoryAdapter.java
@@ -6,8 +6,12 @@ import com.dev.codeinbook.common.exception.NotFoundException;
 import com.dev.codeinbook.domain.port.out.BookRepositoryPort;
 import com.dev.codeinbook.infrastructure.model.Book;
 import com.dev.codeinbook.infrastructure.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -15,6 +19,10 @@ import java.util.Optional;
 public class BookRepositoryAdapter implements BookRepositoryPort {
 
     private BookRepository bookRepository;
+
+    public BookRepositoryAdapter(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     @Override
     public BookDTO create(BookDTO dto) {
@@ -25,7 +33,9 @@ public class BookRepositoryAdapter implements BookRepositoryPort {
 
     @Override
     public void udpate(String id, BookDTO dto) {
-
+        Book book = getBook(id);
+        book.with(dto);
+        bookRepository.save(book);
     }
 
     @Override
@@ -35,12 +45,27 @@ public class BookRepositoryAdapter implements BookRepositoryPort {
 
     @Override
     public void delete(String id) {
+        getBook(id);
+        bookRepository.deleteById(id);
 
     }
 
     @Override
     public PageDTO<BookDTO> findAll(int page, int size) {
-        return null;
+        PageRequest  pageRequest= PageRequest.of(page, size);
+        Page<Book> bookPage = bookRepository.findAll(pageRequest);
+        List<BookDTO> bookList =
+                bookPage.stream()
+                        .map(Book::toBookDTO)
+                        .toList();
+
+        return PageDTO.<BookDTO>builder()
+                .content(bookList)
+                .page(page)
+                .size(size)
+                .totalElements(bookPage.getTotalElements())
+                .totalPages(bookPage.getTotalPages())
+                .build();
     }
 
 
